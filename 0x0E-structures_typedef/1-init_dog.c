cd02480fed8c33216c594348756ff9abcc4be79d
Mayorkay15 <mayorkay15@gmail.com>
2022-07-05 18:14:57 -0700

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL);
+	{ 
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
