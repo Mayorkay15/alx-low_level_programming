be693f7daeb7ed260136acc5370af02a41ae0305
Mayorkay15 <mayorkay15@gmail.com>
2022-07-08 19:40:35 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+        return (-1);
+}
