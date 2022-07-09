899d1d0c42b98e9c3f39b7d2ea5b923e124adb69
Mayorkay15 <mayorkay15@gmail.com>
2022-07-08 19:52:40 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	for (index = 0; index < size; index)
+	for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
