45f05def3d0d9610a136aa224a702ae9ff6430f0
Mayorkay15 <mayorkay15@gmail.com>
2022-07-08 19:28:54 -0700

    array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
