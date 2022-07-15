75e69d7ed9c7c39702581b3ec3a65e1047858e13
Arogbodo Mayowa <mayorkay15@gmail.com>
2022-07-14 19:12:45 -0700

    free-list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
